16bb0b1b444f263ab6dba994067f12897f46a31d
火线兔 <silverhawk@foxmail.com>
2022-04-29 21:27:59 +0800

    TuuzPC

--- app/v1/excel/controller/Index.php
+++ app/v1/excel/controller/Index.php
@@ -3,6 +3,10 @@
 namespace app\v1\excel\controller;
 
 
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use think\facade\Request;
+use think\facade\Validate;
+
 class Index
 {
 
@@ -10,6 +14,22 @@ class Index
 
     public function index()
     {
+        $file = Request::file("file");
+        if (!$file) {
+            \Ret::fail('file字段没有用文件提交');
+        }
+        $hash = $file->hash('md5');
+        if (!Validate::fileExt($file, ["xls", "xlsx"])) {
+            \Ret::fail("ext not allow");
+            return;
+        }
+        if (!Validate::fileSize($file, (float)4 * 1024)) {
+            \Ret::fail("size too big");
+            return;
+        }
+
+        $info = $file->move('./upload/' . $this->token, $hash . "." . $file->getOriginalExtension());
+        $reader = IOFactory::load($info->getPathname());
 
     }
 
