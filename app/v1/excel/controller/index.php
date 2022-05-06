5fb003015c36f6004f3c98eb9fce72e57f1ff730
Tuuz <silverhawk@foxmail.com>
2022-05-06 10:04:29 +0800

    TuuzMac

--- app/v1/excel/controller/index.php
+++ app/v1/excel/controller/index.php
@@ -74,4 +74,47 @@ class index
         echo json_encode($colums);
     }
 
+    public function force()
+    {
+        $token = $this->token;
+        $proc = ProjectModel::api_find_token($token);
+        if (!$proc) {
+            \Ret::fail('项目不可用');
+            return;
+        }
+        $file = Request::file("file");
+        if (!$file) {
+            \Ret::fail('file字段没有用文件提交');
+            return;
+        }
+        $hash = $file->hash('md5');
+        if (!Validate::fileExt($file, ["xls", "xlsx"])) {
+            \Ret::fail("ext not allow");
+            return;
+        }
+        if (!Validate::fileSize($file, (float)8192 * 1024)) {
+            \Ret::fail("size too big");
+            return;
+        }
+
+        $info = $file->move('./upload/excel', $hash . "." . $file->getOriginalExtension());
+        $reader = IOFactory::load($info->getPathname());
+        unlink($info->getPathname());
+        $datas = $reader->getActiveSheet()->toArray();
+        if (count($datas) < 2) {
+            \Ret::fail("表格长度不足");
+            return;
+        }
+        $keys = $datas[0];
+        $count_column = count($keys);
+        $colums = [];
+        for ($i = 1; $i < count($datas); $i++) {
+            $line = $datas[$i];
+            for ($s = 0; $s < $count_column; $s++) {
+                $arr[$keys[$s]] = $line[$s];
+            }
+            $colums[] = $arr;
+        }
+        echo json_encode($colums);
+    }
 }
