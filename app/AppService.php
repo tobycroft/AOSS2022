2efd939ae70f18d311a0b1fb2d4ba5e385f569d1
Tuuz <silverhawk@foxmail.com>
2022-05-07 16:47:00 +0800

    TuuzMac

--- app/AppService.php
+++ app/AppService.php
@@ -20,8 +20,8 @@ class AppService extends Service
         header("Access-Control-Allow-Origin: *", true);
 //        header("access-control-allow-origin: *", true);
         header("Access-Control-Allow-Credentials: true", true);
-//        header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE", true);
-//        header("Access-Control-Allow-Headers: Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-Requested-With", true);
+        header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE", true);
+        header("Access-Control-Allow-Headers: *", true);
         // 服务启动
     }
 }
