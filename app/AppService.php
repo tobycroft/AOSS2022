74f92ac80696823f5d4b3b6ce372b831308f8ee7
Tuuz <silverhawk@foxmail.com>
2022-05-07 16:39:52 +0800

    TuuzMac

--- app/AppService.php
+++ app/AppService.php
@@ -17,7 +17,7 @@ class AppService extends Service
 
     public function boot()
     {
-        header("Access-Control-Allow-Origin: 192.168.110.164", true);
+        header("Access-Control-Allow-Origin: *", true);
 //        header("access-control-allow-origin: *", true);
         header("Access-Control-Allow-Credentials: true", true);
         header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE", true);
