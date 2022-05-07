aad52664ad7730c69e5c1e84524ae7045c4b50e0
Tuuz <silverhawk@foxmail.com>
2022-05-07 16:17:14 +0800

    TuuzMac

--- route/app.php
+++ route/app.php
@@ -14,6 +14,7 @@ use think\facade\Route;
 Route::any(':version/:module/:controller/:function', '\app\:version\:module\controller\:controller@:function')->allowCrossDomain([
     'Access-Control-Allow-Origin' => '*',
     'Access-Control-Allow-Credentials' => 'true',
+    "Access-Control-Allow-Methods" => ["GET", "POST", "PATCH", "PUT", "DELETE"],
 ]);
 
 
