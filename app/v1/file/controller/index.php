32fda3d5b60cd27cf062965dd6ac2b390f643a30
Tuuz <silverhawk@foxmail.com>
2022-05-07 16:14:13 +0800

    TuuzMac

--- app/v1/file/controller/index.php
+++ app/v1/file/controller/index.php
@@ -16,6 +16,7 @@ class index
 
     public function __construct()
     {
+        header("Access-Control-Allow-Origin:*", true);
         $this->token = input('get.token');
         if (!$this->token) {
             \Ret::fail('token');
