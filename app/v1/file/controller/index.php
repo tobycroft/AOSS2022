0c1dbcaca8e1b285cc2ba1bba98388041feb6445
Tuuz <silverhawk@foxmail.com>
2022-05-13 17:18:02 +0800

    TuuzMac

--- app/v1/file/controller/index.php
+++ app/v1/file/controller/index.php
@@ -83,13 +83,13 @@ class index
         }
         if ($proc["type"] == "oss" || $proc["type"] == "all") {
             $oss = new \OSS\AliyunOSS($proc);
-            $oss->uploadFile($proc['bucket'], 'upload/' . $this->token . "/" . $savePath . "/" . $hash . "." . $file->getOriginalExtension(), $fileName);
-            if ($proc['main_type'] == 'oss') {
-                $sav = ($full ? $proc['url'] . '/' : '') . $fileName;
-            }
-            if ($proc["type"] != "all") {
-                unlink($info->getPathname());
-            }
+//            $oss->uploadFile($proc['bucket'], 'upload/' . $this->token . "/" . $savePath . "/" . $hash . "." . $file->getOriginalExtension(), $fileName);
+//            if ($proc['main_type'] == 'oss') {
+//                $sav = ($full ? $proc['url'] . '/' : '') . $fileName;
+//            }
+//            if ($proc["type"] != "all") {
+//                unlink($info->getPathname());
+//            }
         }
 
         AttachmentModel::create($file_info);
